777bf9934d658576d0b88f99d1b04fed1d0003f0
AtalaySezen <atalaysezen@yahoo.com.tr>
2022-07-29 15:41:24 +0300

    new project created, angular material added.

---
+++ notepad-app/src/app/quicknotes/quicknotes.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-quicknotes',
+  templateUrl: './quicknotes.component.html',
+  styleUrls: ['./quicknotes.component.css']
+})
+export class QuicknotesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
