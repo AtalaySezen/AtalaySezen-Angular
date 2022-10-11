d353e819344bbe6671be809003f583afa9c2d01e
Atalay Sezen <88587309+AtalaySezen@users.noreply.github.com>
2022-10-11 13:39:01 +0300

    boards component localstorage

--- angular-Jiraproject/src/app/components/board/boards-dialog/boards-dialog.component.ts
+++ angular-Jiraproject/src/app/components/board/boards-dialog/boards-dialog.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Form, FormControl, FormGroup, Validators } from '@angular/forms';
 import { MatDialogRef } from '@angular/material/dialog';
+import { BoardService } from 'src/app/services/board.service';
+
 @Component({
   selector: 'app-boards-dialog',
   templateUrl: './boards-dialog.component.html',
@@ -13,13 +15,13 @@ export class BoardsDialogComponent implements OnInit {
     title: new FormControl(null, [Validators.required])
   })
 
-  constructor(private dialogRef: MatDialogRef<BoardsDialogComponent>) { }
+  constructor(private dialogRef: MatDialogRef<BoardsDialogComponent>, private boardService: BoardService) { }
 
   ngOnInit(): void {
   }
 
   create() {
-
+    this.boardService.createBoard(this.boardForm.get('title')?.value)
   }
 
   close() {
---
+++ angular-Jiraproject/src/app/services/board.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BoardService {
+  public boards: Array<any> = [];
+
+  constructor() {
+    let str = localStorage.getItem('boards');
+    if (str != null) {
+      this.boards = JSON.parse(str);
+    }
+  }
+
+  public createBoard(title: any) {
+    let newBoardObject = {
+      title: title,
+      cards: []
+    };
+
+    this.boards.push(newBoardObject);
+    localStorage.setItem('boards', JSON.stringify(this.boards));
+    console.log(this.boards);
+  }
+
+
+
+
+
+
+}
